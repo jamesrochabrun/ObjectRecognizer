eecdab13eb2ec433d43c89aa651726a229e598d8
James Rochabrun <j@Jamess-MacBook-Pro-2.local>
2018-04-15 20:45:43 -0700

    first commit

---
+++ ObjectsIdentifier/CGImagePropertyOrientation.swift
@@ -0,0 +1,34 @@
+//
+//  CGImagePropertyOrientation.swift
+//  ObjectsIdentifier
+//
+//  Created by james rochabrun on 4/13/18.
+//  Copyright © 2018 james rochabrun. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+import ImageIO
+
+extension CGImagePropertyOrientation {
+    /**
+     Converts a `UIImageOrientation` to a corresponding
+     `CGImagePropertyOrientation`. The cases for each
+     orientation are represented by different raw values.
+     
+     - Tag: ConvertOrientation
+     */
+    init(_ orientation: UIImageOrientation) {
+        switch orientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
