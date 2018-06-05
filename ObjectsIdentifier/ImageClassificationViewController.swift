4dbb7c6ab9e0eb970c3fcfd00c700c081b513341
James Rochabrun <j@Jamess-MacBook-Pro-3.local>
2018-06-04 21:40:23 -0700

    first create ml model

--- ObjectsIdentifier/ImageClassificationViewController.swift
+++ ObjectsIdentifier/ImageClassificationViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 import CoreML
 import Vision
 import ImageIO
+import Photos
 
 class ImageClassificationViewController: UIViewController {
     // MARK: - IBOutlets
@@ -35,7 +36,7 @@ class ImageClassificationViewController: UIViewController {
     lazy var classificationRequest: VNCoreMLRequest = {
         do {
 
-            let model = try VNCoreMLModel(for: wedding().model)
+            let model = try VNCoreMLModel(for: ImageClassifier().model)
             
             let request = VNCoreMLRequest(model: model, completionHandler: { [weak self] request, error in
                 self?.processClassifications(for: request, error: error)
@@ -70,7 +71,7 @@ class ImageClassificationViewController: UIViewController {
     func processClassifications(for request: VNRequest, error: Error?) {
         DispatchQueue.main.async {
             guard let results = request.results else {
-                self.classificationLabel.text = "Unable to classify image.\n\(error!.localizedDescription)"
+                self.classificationLabel.text = "Unable to classify image.\n\(String(describing: error?.localizedDescription))"
                 return
             }
             // The `results` will always be `VNClassificationObservation`s, as specified by the Core ML model in this project.
@@ -128,9 +129,22 @@ extension ImageClassificationViewController: UIImagePickerControllerDelegate, UI
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String: Any]) {
         picker.dismiss(animated: true)
         
+        if let asset = info[UIImagePickerControllerPHAsset] as? PHAsset  {
+            
+            print("KMPHASSET \(asset.localIdentifier)/")
+            print("KMPHASSET \(asset.creationDate)/")
+
+        }
+        
         // We always expect `imagePickerController(:didFinishPickingMediaWithInfo:)` to supply the original image.
         let image = info[UIImagePickerControllerOriginalImage] as! UIImage
         imageView.image = image
         updateClassifications(for: image)
     }
 }
+
+
+
+
+
+
